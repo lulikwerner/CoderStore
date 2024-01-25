cd09ad3251de94b7b9d585f805de72a633ab0fae
Lucila Werner <lulikwerner@gmail.com>
2024-01-25 18:45:40 -0500

    pyhton v

--- coderstore/settings.py
+++ coderstore/settings.py
@@ -130,8 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'statifiles'),)
 # img
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
--- coderstore/urls.py
+++ coderstore/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('about/', about_us, name='about'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
