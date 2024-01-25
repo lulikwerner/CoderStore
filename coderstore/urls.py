e663432ac019e7aada1a3c3a1dd78e51ce00af58
Lucila Werner <lulikwerner@gmail.com>
2024-01-25 18:24:11 -0500

    static media root

--- coderstore/urls.py
+++ coderstore/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from coderstore.views import all_products, home_page, search_products, index, about_us
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,12 +10,15 @@ urlpatterns = [
     path('meat/', include('meat.urls')),
     path('drink/', include('drink.urls')),
     path('bakery/', include('bakery.urls')),
-    path('search/',search_products, name='search-products'),
+    path('search/', search_products, name='search-products'),
     path('inicio/', home_page, name='inicio'),
     path('user/', include('users.urls')),
     path('contact/', include('contact.urls')),
     path('all/', all_products, name='all_products'),
     path('stock/', include('stock.urls')),
     path('cart/', include('cart.urls')),
-    path('about/', about_us, name='about')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('about/', about_us, name='about'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
