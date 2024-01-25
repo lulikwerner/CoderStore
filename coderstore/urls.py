570830f1af47bdfcf75f66d32eec7c9212bacda0
Lucila Werner <lulikwerner@gmail.com>
2024-01-25 18:17:08 -0500

    static media root

--- coderstore/urls.py
+++ coderstore/urls.py
@@ -18,7 +18,4 @@ urlpatterns = [
     path('stock/', include('stock.urls')),
     path('cart/', include('cart.urls')),
     path('about/', about_us, name='about')
-]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
