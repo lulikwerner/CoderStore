4e5fd8442927d9602e9a66c52e26d9efb0f8aeb7
Lucila Werner <lulikwerner@gmail.com>
2024-02-01 12:27:24 -0500

    typo

--- coderstore/settings.py
+++ coderstore/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'coderstore.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 if not DEBUG:
-    DATABASE={
+    DATABASES={
         "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 else:
