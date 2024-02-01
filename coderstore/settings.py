616ec6a37a012f5701fa40a5fc74733c8709b6b6
Lucila Werner <lulikwerner@gmail.com>
2024-02-01 11:58:53 -0500

    allow_host

--- coderstore/settings.py
+++ coderstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&jce&h02e^ol6h$f&s=rx61#0q$40o)#jgs^00!pabooduf#tv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','coderstore.onrender.com']
 
 
 # Application definition
