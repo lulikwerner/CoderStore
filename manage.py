82e14977df1a1296acc29c719e8ee1f4719c1c90
Lucila Werner <lulikwerner@gmail.com>
2024-02-01 10:46:11 -0500

    render

--- manage.py
+++ manage.py
@@ -1,13 +1,19 @@
-#!/usr/bin/env python
+
+    #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+# Agregado
+import django
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coderstore.settings')
     try:
+        # Override default port for `runserver` command
+        from django.core.management.commands.runserver import Command as runserver
+        runserver.default_port = "8080"
+        # Hasta aquí agregado
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
@@ -16,7 +22,9 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
+    django.setup()
 
 if __name__ == '__main__':
     main()
+
+    
