bd4694a650d53d26111fcb06c8768f8acd67bd65
Lucila Werner <lulikwerner@gmail.com>
2024-02-01 12:18:55 -0500

    db SECRET_KEY

--- coderstore/settings.py
+++ coderstore/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&jce&h02e^ol6h$f&s=rx61#0q$40o)#jgs^00!pabooduf#tv'
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-&jce&h02e^ol6h$f&s=rx61#0q$40o)#jgs^00!pabooduf#tv')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG','True')=="True"
