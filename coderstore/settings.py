1e16582ecceb0cb78f5faf3f3e682b0d693944eb
Lucila Werner <lulikwerner@gmail.com>
2024-02-01 13:06:19 -0500

    static

--- coderstore/settings.py
+++ coderstore/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
-STATIC_ROOT = BASE_DIR/'assets'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')#
+#STATIC_ROOT = BASE_DIR/'assets'#
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # img
